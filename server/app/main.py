935187c1a20bdeb57bca4653b08441b6a8c1aea6
abujawed11 <abubakar.jawed@gmail.com>
2025-09-04 22:03:41 +0530

    added cors

--- server/app/main.py
+++ server/app/main.py
@@ -13,7 +13,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://10.20.2.78:5173", # WSL IP
-        "http://10.20.2.78:5174"
+        "http://10.20.2.78:5174",
+        "http://93.127.199.118",
     ],
     allow_credentials=True,
     allow_methods=["*"],
